bc6235856cdf15d548a93b0e96ef31a5345b6f33
Zhalgas <zhalgas.bagytzhan@mail.ru>
2024-02-24 22:42:07 +0600

    Sample

--- main.go
+++ main.go
@@ -17,7 +17,6 @@ func main() {
 	teams := []string{"Team 1", "Team 2", "Team 3", "Team 4", "Team 5", "Team 6", "Team 7", "Team 8"}
 
 	rand.Seed(time.Now().UnixNano())
-
 	rand.Shuffle(len(teams), func(i, j int) {
 		teams[i], teams[j] = teams[j], teams[i]
 	})
@@ -28,13 +27,44 @@ func main() {
 		quarterFinals = append(quarterFinals, match)
 	}
 
-	for i := range quarterFinals {
-		quarterFinals[i].Score1 = rand.Intn(5)
-		quarterFinals[i].Score2 = rand.Intn(5)
-	}
+	var winners []string
+	var drawers []Match // Сохраняем матчи, которые закончились вничью
 
 	fmt.Println("Quarterfinals:")
 	for _, match := range quarterFinals {
+		match.Score1 = rand.Intn(5)
+		match.Score2 = rand.Intn(5)
 		fmt.Printf("%s %d - %d %s\n", match.Team1, match.Score1, match.Score2, match.Team2)
+
+		if match.Score1 == match.Score2 {
+			drawers = append(drawers, match)
+		} else if match.Score1 > match.Score2 {
+			winners = append(winners, match.Team1)
+		} else {
+			winners = append(winners, match.Team2)
+		}
+	}
+
+	fmt.Println("Rematches for draws:")
+	for _, drawMatch := range drawers {
+		for {
+			drawMatch.Score1 = rand.Intn(5)
+			drawMatch.Score2 = rand.Intn(5)
+			fmt.Printf("%s %d - %d %s\n", drawMatch.Team1, drawMatch.Score1, drawMatch.Score2, drawMatch.Team2)
+
+			if drawMatch.Score1 != drawMatch.Score2 {
+				if drawMatch.Score1 > drawMatch.Score2 {
+					winners = append(winners, drawMatch.Team1)
+				} else {
+					winners = append(winners, drawMatch.Team2)
+				}
+				break
+			}
+		}
+	}
+
+	fmt.Println("Winners of the quarterfinals:")
+	for _, winner := range winners {
+		fmt.Println(winner)
 	}
 }
