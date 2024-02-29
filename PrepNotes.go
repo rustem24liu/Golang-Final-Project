7677dda5d59a8dd92704cf09c6b7ead7d86e1280
Ayan <ay_suleimenov@kbtu.kz>
2024-02-29 14:44:07 +0600

    rus , here we GO!

--- PrepNotes.go
+++ PrepNotes.go
@@ -3,5 +3,16 @@
 //(любое ПО со всеми зависимостями и запускать в любой операционной системе.)
 
 
-// Docker-Code ubuntuu
-FROM
+// Terminal Code for building the app "docker-compose.yml" //
+// build -t prod-service:local
+
+// Setup
+// docker compose -f docker-compose.yml up
+// creating "data.dir"
+
+// config must be filled by postgreSQL data.
+
+// host: host.docker.internal - postgresql - post "5432";
+// linux - create bridge
+
+
