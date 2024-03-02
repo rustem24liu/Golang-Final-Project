8c9ed0bd8c2c1dd341c4458aa77a8bd3ce74ce48
Zhalgas <zhalgas.bagytzhan@mail.ru>
2024-03-02 15:52:22 +0600

    sample

--- main.go
+++ main.go
@@ -14,24 +14,24 @@ type Match struct {
 }
 
 func main() {
-	teams := []string{"Team 1", "Team 2", "Team 3", "Team 4", "Team 5", "Team 6", "Team 7", "Team 8"}
+	teams := []string{"Team 1", "Team 2", "Team 3", "Team 4", "Team 5", "Team 6", "Team 7", "Team 8", "Team 9", "Team 10", "Team 11", "Team 12", "Team 13", "Team 14", "Team 15", "Team 16"}
 
 	rand.Seed(time.Now().UnixNano())
 	rand.Shuffle(len(teams), func(i, j int) {
 		teams[i], teams[j] = teams[j], teams[i]
 	})
 
-	quarterFinals := make([]Match, 0)
+	roundOf16 := make([]Match, 0)
 	for i := 0; i < len(teams); i += 2 {
 		match := Match{Team1: teams[i], Team2: teams[i+1]}
-		quarterFinals = append(quarterFinals, match)
+		roundOf16 = append(roundOf16, match)
 	}
 
 	var winners []string
 	var drawers []Match // Сохраняем матчи, которые закончились вничью
 
-	fmt.Println("Quarterfinals:")
-	for _, match := range quarterFinals {
+	fmt.Println("Round of 16:")
+	for _, match := range roundOf16 {
 		match.Score1 = rand.Intn(5)
 		match.Score2 = rand.Intn(5)
 		fmt.Printf("%s %d - %d %s\n", match.Team1, match.Score1, match.Score2, match.Team2)
@@ -63,7 +63,7 @@ func main() {
 		}
 	}
 
-	fmt.Println("Winners of the quarterfinals:")
+	fmt.Println("Winners of the round of 16:")
 	for _, winner := range winners {
 		fmt.Println(winner)
 	}
