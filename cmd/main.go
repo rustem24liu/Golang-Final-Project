a0e3d7c972640d29f238718676d07aab913628f0
rustem24liu <temirgalirustem05@gmail.com>
2024-03-06 00:15:00 +0500

    last update for 05.03.24

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	_ "fmt"
 	_ "github.com/lib/pq"
+	"github.com/rustem24liu/Golang-Final-Project/database"
 	"github.com/rustem24liu/Golang-Final-Project/internal/tournament"
 	_ "github.com/rustem24liu/Golang-Final-Project/internal/tournament"
 )
@@ -11,4 +12,10 @@ import (
 func main() {
 	tournament.RunTournament()
 	fmt.Println("Tournament finished")
+
+	rows, err := database.GetTeamNames()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(rows)
 }
